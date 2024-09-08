8f10edc4d21c528c7e51da050fc8f8a792c3d764
Arghyajit Sarkar <153011594+Arghyajit77@users.noreply.github.com>
2024-09-08 12:53:26 +0530

    Final
    Gym Managemnet Using Apache NetBeans

---
+++ src/createpage/JFileChooser.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package createpage;
+
+import java.io.File;
+
+/**
+ *
+ * @author ARGHYAJIT
+ */
+class JFileChooser {
+
+    void showOpenDialog(String anull) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    File getSelectedFile() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
